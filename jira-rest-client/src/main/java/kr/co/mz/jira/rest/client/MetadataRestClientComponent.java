67da34a193596354dae95f1af0f6762a92e131f2
JinBum Jeong <mz_gogosing@woowafrineds.com>
2023-01-31 08:50:41 +0900

    [WIP] 엑셀 문서 생성 구현 중

--- jira-rest-client/src/main/java/kr/co/mz/jira/rest/client/MetadataRestClientComponent.java
+++ jira-rest-client/src/main/java/kr/co/mz/jira/rest/client/MetadataRestClientComponent.java
@@ -2,6 +2,7 @@ package kr.co.mz.jira.rest.client;
 
 import com.atlassian.jira.rest.client.api.MetadataRestClient;
 import com.atlassian.jira.rest.client.api.domain.Field;
+import com.atlassian.jira.rest.client.api.domain.Status;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -16,11 +17,19 @@ public class MetadataRestClientComponent {
 
   private final MetadataRestClient metadataRestClient;
 
-  public Set<Field> loadAll() {
+  public Set<Field> loadAllFields() {
     final var fields = metadataRestClient.getFields().claim();
 
     return StreamSupport
         .stream(fields.spliterator(), false)
+        .collect(Collectors.toSet());
+  }
+
+  public Set<Status> loadAllStatuses() {
+    final var statuses = metadataRestClient.getStatuses().claim();
+
+    return StreamSupport
+        .stream(statuses.spliterator(), false)
         .collect(Collectors.toSet());
   }
 }

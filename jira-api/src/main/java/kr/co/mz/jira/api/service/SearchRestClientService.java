83c4ef4e17c2fadd5b552f29fa08044ab7501e3c
JinBum Jeong <mz_gogosing@woowafrineds.com>
2023-03-17 12:35:09 +0900

    [NO-JIRA] 최대 조회 가능 수량 조정

--- jira-api/src/main/java/kr/co/mz/jira/api/service/SearchRestClientService.java
+++ jira-api/src/main/java/kr/co/mz/jira/api/service/SearchRestClientService.java
@@ -12,11 +12,22 @@ import org.springframework.validation.annotation.Validated;
 @RequiredArgsConstructor
 public class SearchRestClientService {
 
+  /**
+   * 검색어(jql)결과의 조회 총 수량 제한 값.
+   * (JIRA SDK 에서 최대 허용 가능한 수량은 1500이다. 만약, 초과할 경우 1500으로 강제 조정된다.)
+   */
+  private static final Integer FETCH_SEARCH_MAX_RESULT_LIMIT = 1500;
+
   private final SearchRestClient searchRestClient;
 
   @SuppressWarnings("UnstableApiUsage")
   public SearchResult loadByJql(final @NotBlank String jql) {
-    final var promiseSearchResult = searchRestClient.searchJql(jql);
+    final var promiseSearchResult = searchRestClient.searchJql(
+        jql,
+        FETCH_SEARCH_MAX_RESULT_LIMIT,
+        null,
+        null
+    );
 
     return promiseSearchResult.claim();
   }
